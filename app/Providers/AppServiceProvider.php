d104fbbd3dc0a6f99fd060745f4638e262601df9
manisha-propel <mmanisha@propelsoft.in>
2024-09-27 15:24:50 +0530

    Initial commit of my Laravel package

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,26 +6,28 @@ use Illuminate\Support\ServiceProvider;
 use ManishaPropel\InterfaceRepository\Interfaces\PersonInterface;
 use ManishaPropel\InterfaceRepository\Repositories\PersonRepository;
 
-
-
 class AppServiceProvider extends ServiceProvider
 {
     /**
      * Register any application services.
+     *
+     * This method is called when the application is bootstrapping services.
+     * You can bind interfaces to their implementations here.
      */
-    // public function register(): void
-    // {
-    //     //
-    // }
     public function register()
     {
+        // Bind the PersonInterface to the PersonRepository implementation
         $this->app->bind(PersonInterface::class, PersonRepository::class);
     }
+
     /**
      * Bootstrap any application services.
+     *
+     * This method is called after all services are registered.
+     * You can perform any additional setup here if needed.
      */
-    public function boot(): void
+    public function boot()
     {
-        //
+        // You can add bootstrapping logic here if required
     }
 }
