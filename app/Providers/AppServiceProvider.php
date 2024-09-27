e4ad97ef6088cd53934a7d80bd3a38fa8d279789
manisha-propel <mmanisha@propelsoft.in>
2024-09-27 09:56:10 +0530

    Added interfaces and repositories

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,17 +3,24 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use ManishaPropel\InterfaceRepository\Interfaces\PersonInterface;
+use ManishaPropel\InterfaceRepository\Repositories\PersonRepository;
+
+
 
 class AppServiceProvider extends ServiceProvider
 {
     /**
      * Register any application services.
      */
-    public function register(): void
+    // public function register(): void
+    // {
+    //     //
+    // }
+    public function register()
     {
-        //
+        $this->app->bind(PersonInterface::class, PersonRepository::class);
     }
-
     /**
      * Bootstrap any application services.
      */
